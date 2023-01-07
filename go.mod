7b2a06d0e58940e6f3df0255788b95972bb41ee5
mathew <meta@pobox.com>
2023-01-06 20:20:21 -0600

    Cleanup for 2023

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module mviplayer
 go 1.15
 
 require (
-	github.com/adrg/xdg v0.2.3
-	github.com/dhowden/tag v0.0.0-20201120070457-d52dcb253c63
+	github.com/adrg/xdg v0.4.0
+	github.com/dhowden/tag v0.0.0-20220618230019-adf36e896086
+	golang.org/x/sys v0.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,36 +4,30 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
 	"unicode"
 
-  "github.com/adrg/xdg"
+	"github.com/adrg/xdg"
 	"github.com/dhowden/tag"
 )
 
-func printHelp() {
-	fmt.Println(`mviplayer [FLAGS] FILES DIR`)
-	fmt.Println()
-	fmt.Println(`Moves all m4a files under FILES to be under destination DIR, renaming them`)
-	fmt.Println(`according to their metadata.`)
-	fmt.Println()
-	flag.PrintDefaults()
-}
-
 var dryRun = flag.Bool("t", false, "test and report what would happen without doing it (implies -v)")
-var help = flag.Bool("h", false, "show help text")
 var verbose = flag.Bool("v", false, "verbose mode, report activity")
 
 func main() {
-
+	flag.Usage = func() {
+		cmdname := filepath.Base(os.Args[0])
+		fmt.Fprintf(os.Stderr, "%s - file .m4a files downloaded from BBC iPlayer\nusage: %s [FLAGS] FILES DIR\n", cmdname, cmdname)
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
-	if *help || len(os.Args) < 3 {
-		printHelp()
+	if len(os.Args) < 3 {
+		flag.Usage()
 		return
 	}
 
@@ -48,7 +42,7 @@ func main() {
 
 	args := flag.Args()
 
-	destDir := args[len(args) - 1]
+	destDir := args[len(args)-1]
 	dirInfo, err := os.Stat(destDir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "can't move files to %s: %v\n", destDir, err)
@@ -64,16 +58,16 @@ func main() {
 		fmt.Fprintf(os.Stderr, "can't read rename rules: %v\n", err)
 	}
 
-	for _, fspc := range args[0:len(args)-1] {
-		err := filepath.Walk(fspc, renamer(destDir, rules))
+	for _, fspc := range args[0 : len(args)-1] {
+		err := filepath.WalkDir(fspc, renamer(destDir, rules))
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s: %v\n", fspc, err)
 		}
 	}
 }
 
-func renamer(dest string, rules []RenameRule) filepath.WalkFunc {
-	return func (path string, info os.FileInfo, err error) error {
+func renamer(dest string, rules []RenameRule) fs.WalkDirFunc {
+	return func(path string, info fs.DirEntry, err error) error {
 		// No special processing needed for errors
 		if err != nil {
 			return err
@@ -84,7 +78,7 @@ func renamer(dest string, rules []RenameRule) filepath.WalkFunc {
 		}
 		// Skip macOS crud
 		name := filepath.Base(path)
-		if name == ".DS_Store" || name == "Icon\r"{
+		if name == ".DS_Store" || name == "Icon\r" {
 			return nil
 		}
 		ext := filepath.Ext(path)
@@ -117,9 +111,9 @@ func renamer(dest string, rules []RenameRule) filepath.WalkFunc {
 func applyRules(rules []RenameRule, x string) string {
 	for i, r := range rules {
 		xb := x
-    if r.FromRE == nil {
-      panic(fmt.Sprintf("regexp %d didn't get compiled", i))
-    }
+		if r.FromRE == nil {
+			panic(fmt.Sprintf("regexp %d didn't get compiled", i))
+		}
 		x = r.FromRE.ReplaceAllString(x, r.To)
 		if *verbose && xb != x {
 			fmt.Printf("rule %d changed %s to %s\n", i+1, xb, x)
@@ -129,29 +123,29 @@ func applyRules(rules []RenameRule, x string) string {
 }
 
 func renameFile(path string, dest string, rules []RenameRule, tags tag.Metadata) error {
-  ext := filepath.Ext(path)
+	ext := filepath.Ext(path)
 	enn, _ := tags.Track()
 	snn, _ := tags.Disc()
 	title := sanitize(applyRules(rules, tags.Title()))
 	show := sanitize(applyRules(rules, tags.Album()))
 	fname := fmt.Sprintf("s%02d e%02d %s%s", snn, enn, title, ext)
 	destpath := filepath.Join(dest, show, fname)
-  destdir := filepath.Dir(destpath)
-  err := os.MkdirAll(destdir, 0755)
-  if err != nil {
-    return err
-  }
-  if *verbose {
-    fmt.Printf("move %s to %s\n", path, destpath)
-  }
-  if !*dryRun {
-    err := os.Rename(path, destpath)
-    return err
-  }
+	destdir := filepath.Dir(destpath)
+	err := os.MkdirAll(destdir, 0755)
+	if err != nil {
+		return err
+	}
+	if *verbose {
+		fmt.Printf("move %s to %s\n", path, destpath)
+	}
+	if !*dryRun {
+		err := os.Rename(path, destpath)
+		return err
+	}
 	return nil
 }
 
-const okRunes =`!#$%&'(),-= `
+const okRunes = `!#$%&'(),-= `
 
 // Sanitize to a filename
 func sanitize(title string) string {
@@ -166,16 +160,16 @@ func sanitize(title string) string {
 
 type RenameRule struct {
 	FromRE *regexp.Regexp
-	From string `json:"from"`
-	To string `json:"to"`
+	From   string `json:"from"`
+	To     string `json:"to"`
 }
 
 func readRules(destDir string) ([]RenameRule, error) {
 	ruleFile, err := xdg.ConfigFile("mviplayer/rules.json")
-  if err != nil {
-    return []RenameRule{}, err
-  }
-	data, err := ioutil.ReadFile(ruleFile)
+	if err != nil {
+		return []RenameRule{}, err
+	}
+	data, err := os.ReadFile(ruleFile)
 	if err != nil {
 		if os.IsNotExist(err) {
 			if *verbose {
@@ -187,12 +181,12 @@ func readRules(destDir string) ([]RenameRule, error) {
 	if *verbose {
 		fmt.Printf("reading rename rules from %s\n", destDir)
 	}
-	rules := []RenameRule{}
+	var rules []RenameRule
 	err = json.Unmarshal(data, &rules)
 	if err != nil {
 		return rules, err
 	}
-	for i,r := range rules {
+	for i, r := range rules {
 		rules[i].FromRE, err = regexp.Compile(r.From)
 		if err != nil {
 			return []RenameRule{}, err
